c77fad2a2265058adc88ebf1ee0c1fca6eefabf8
Gram Stone <stonegram@gmail.com>
2021-12-30 22:07:54 -0500

    Add efficient solution to maxMin, and incorrect WIP binary search version

--- maxMin.js
+++ maxMin.js
@@ -1,9 +1,71 @@
+// Naive brute force algorithm
 function maxMin(k, arr) {
-  const sorted = arr.sort((m, n) => m - n);
+  const sortedArr = arr.sort((m, n) => m - n);
   let min = Infinity;
   for (let i = 0; i + k <= arr.length; i++) {
-    const slice = sorted.slice(i, i + k);
-    min = Math.min(min, slice[k - 1] - slice[0]);
+    min = Math.min(min, sortedArr[i + k - 1] - sortedArr[i]);
   }
   return min;
+}
+
+// Binary search version that is failing a few tests
+function maxMinBinarySearch(k, arr) {
+  const sortedArr = arr.sort((m, n) => m - n);
+
+  if (sortedArr[arr.length - 1] - sortedArr[0] == 0) {
+    return 0;
+  }
+
+  function bruteForceSearch(firstIndex, secondIndex) {
+    const length = secondIndex - firstIndex + 1;
+    let min = Infinity;
+    for (let i = 0; i + k <= length; i++) {
+      const delta =
+        sortedArr[firstIndex + i + k - 1] - sortedArr[firstIndex + i];
+      if (delta == 0) {
+        return 0;
+      } else {
+        min = Math.min(min, delta);
+      }
+    }
+    return min;
+  }
+
+  function binarySearch(firstIndex, secondIndex) {
+    const length = secondIndex - firstIndex + 1;
+    if (length > 2 * k) {
+      const midpoint = length % 2 == 0 ? length / 2 : Math.floor(length / 2);
+      const [firstHalf, secondHalf, miss] = [
+        [firstIndex, midpoint],
+        [midpoint + 1, secondIndex],
+        [midpoint - k - 1, midpoint + k - 2],
+      ];
+      let [deltaFirstHalf, deltaSecondHalf] = [
+        sortedArr[firstHalf[1]] - sortedArr[firstHalf[0]],
+        sortedArr[secondHalf[1]] - sortedArr[secondHalf[0]],
+      ];
+      if (deltaFirstHalf == 0 || deltaSecondHalf == 0) {
+        return 0;
+      } else if (deltaFirstHalf == deltaSecondHalf) {
+        return [
+          bruteForceSearch(...miss),
+          binarySearch(...firstHalf),
+          binarySearch(...secondHalf),
+        ];
+      } else if (deltaFirstHalf < deltaSecondHalf) {
+        return [bruteForceSearch(...miss), binarySearch(...firstHalf)];
+      } else {
+        return [bruteForceSearch(...miss), binarySearch(...secondHalf)];
+      }
+    } else {
+      return bruteForceSearch(firstIndex, secondIndex);
+    }
+  }
+
+  const result = binarySearch(0, arr.length - 1);
+  if (Number.isInteger(result)) {
+    return result;
+  } else {
+    return Math.min(...result.flat(Infinity));
+  }
 }
